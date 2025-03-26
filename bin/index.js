29ab8eebfc1a65a5e9420f08111c20f395855c08
glennli <glennli@lexin.com>
2025-03-26 10:12:22 +0800

    refactor: 将项目从ESM迁移到CommonJS
    重构项目模块系统，从ESM迁移到CommonJS，修改了模块导入导出方式及相关配置。同时添加了chalk依赖用于命令行输出美化，并新增了显示版本号的功能。

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-// 调试 node bin/index.js extract
+// 调试 node bin/index.js extract 或者 在业务工程下执行 pnpm link 该包的根目录path
 
-import { program } from 'commander';
-// import packageInfo from '../package.json' assert { type: 'json' };
-import { extract } from '../lib/cli/extract.js';
+const { program } = require('commander');
+const { extract } = require('../lib/cli/extract');
+const { version } = require('../package.json');
 
 // 添加 extract 命令
 program
@@ -13,5 +13,23 @@ program
     extract();
   });
 
+// 添加 -v显示版本号
+program.option('-v, --version', '显示版本号').action(() => {
+  chalk('green', version);
+});
+
 // 解析命令行参数
 program.parse(process.argv);
+
+function chalk(color, ...args) {
+  // chalk v5.0.0 版本开始，是ESModule方式导出的，所以使用 import 导入
+  return import('chalk').then(module => {
+    const chalk = module.default;
+    if (chalk[color]) {
+      const coloredText = chalk[color](...args);
+      console.log(coloredText);
+    } else {
+      console.log(...args);
+    }
+  });
+}
