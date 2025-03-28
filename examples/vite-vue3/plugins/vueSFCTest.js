a47e160a85e75c2bbc0de82b72948b70af68e799
glennli <glennli@lexin.com>
2025-03-28 19:42:14 +0800

    添加husky

--- examples/vite-vue3/plugins/vueSFCTest.js
+++ examples/vite-vue3/plugins/vueSFCTest.js
@@ -1,35 +1,32 @@
 // 从vue 3.2.13+开始，vue/compiler-sfc 已经内置了对.vue文件的编译，不需要额外安装
-import {
-  parse as parseVueSFC,
-  compileTemplate,
-  compileScript,
-} from "vue/compiler-sfc";
-import { parse } from "@babel/parser";
-import traversePkg from "@babel/traverse";
-import generatorPkg from "@babel/generator";
-import path from "path";
-import fs from "fs";
+import { parse as parseVueSFC, compileTemplate, compileScript } from 'vue/compiler-sfc';
+import { parse } from '@babel/parser';
+import traversePkg from '@babel/traverse';
+import generatorPkg from '@babel/generator';
+import path from 'path';
+import fs from 'fs';
 
 const traverse = traversePkg.default;
 const generate = generatorPkg.default;
 
-const fileContent = fs.readFileSync(path.resolve("./src/App.vue"), "utf-8");
+// const fileContent = fs.readFileSync(path.resolve("./src/App.vue"), "utf-8");
+const fileContent = fs.readFileSync(path.resolve('./src/components/vue2SyntaxView.vue'), 'utf-8');
 const { descriptor } = parseVueSFC(fileContent);
 
 const { code: templateCode } = compileTemplate({
-  id: "1",
-  filename: "1.vue",
+  id: '1',
+  filename: '1.vue',
   source: descriptor.template.content,
   compilerOptions: {
     // 不保留template中的注释
-    comments: false,
-  },
+    comments: false
+  }
 });
-console.log("\ntemplateCode\n", templateCode);
+console.log('\ntemplateCode\n', templateCode);
 
 const { content: scriptCode } = compileScript(descriptor, {
-  id: "1",
-  filename: "1.vue",
-  sourceMap: false,
+  id: '1',
+  filename: '1.vue',
+  sourceMap: false
 });
-console.log("\nscriptCode\n", scriptCode);
+console.log('\nscriptCode\n', scriptCode);
--- examples/vite-vue3/src/components/vue2SyntaxView.vue
+++ examples/vite-vue3/src/components/vue2SyntaxView.vue
@@ -2,30 +2,38 @@
   <div>
     Vue2SyntaxView
     {{ src }}
-    <div>{{ computedB }}</div>
+    <div>{{ computedB }}啊啊啊</div>
     <div>{{ `${computedB}好啊2` }}</div>
+    <div>{{ methodB() }}</div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  name: "Vue2SyntaxView",
+  name: 'Vue2SyntaxView',
   props: {
     src: {
       type: String,
-      default: "src链接",
-    },
+      default: 'src链接'
+    }
   },
   data() {
     return {
-      a: "字段a",
+      a: '字段a'
     };
   },
   computed: {
     computedB() {
       return `computedB${this.a}啊`;
-    },
+    }
   },
+  methods: {
+    methodB() {
+      return `methodB${this.a}啊`;
+    }
+  }
 };
 </script>
 
