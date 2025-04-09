d2b2cd5923255d05c8b8a4e77d1cbe8ac9235b21
glennli <glennli@lexin.com>
2025-04-09 11:32:23 +0800

    解析js、json

--- examples/vite-vue3/src/main.js
+++ examples/vite-vue3/src/main.js
@@ -1,9 +1,9 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 // import ElementPlus from "element-plus";
 // import "element-plus/dist/index.css";
-
+console.log('hello vite 你好');
 createApp(App)
   // .use(ElementPlus)
-  .mount("#app");
+  .mount('#app');
