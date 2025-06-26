e11a450e99e1422ddef28e243be82fed0811a46e
glennli <glennli@lexin.com>
2025-06-26 19:12:44 +0800

    新增vue2-vite4项目

---
+++ examples/vue2-vite4/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Hello, Vue 2 with Vite!</h1>
+    <p>This is a Vue 2 project powered by Vite.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
